f46dd7611078839ac96f55bfc1714836a250e132
Kewbish <kewbish@gmail.com>
2020-05-14 08:35:51 -0700

    Implement meta change on attribute change

--- index.js
+++ index.js
@@ -10,18 +10,24 @@ class RevShare extends HTMLElement {
         parsedPointers = JSON.parse(parsedPointers);
         document.allPointers.push(parsedPointers);
     }
+    attributeChangedCallback() {
+        genMeta();
+    }
+    static get observedAttributes() {
+        return ['pointers'];
+    }
 }
 
 window.customElements.define('rev-share', RevShare);
 
 function pickPointer() {
     var p = Math.floor(Math.random() * document.allPointers.length);
-    const sum = Object.values(document.allPointers[p]).reduce((sum, weight) => sum + weight, 0)
-    let choice = Math.random() * sum
+    const sum = document.allPointers[p] && Object.values(document.allPointers[p]).reduce((sum, weight) => sum + weight, 0);
+    let choice = Math.random() * sum;
     for (const pointer in document.allPointers[p]) {
-        const weight = document.allPointers[p][pointer]
+        const weight = document.allPointers[p][pointer];
         if ((choice -= weight) <= 0) {
-            return pointer
+            return pointer;
         }
     }
 }
