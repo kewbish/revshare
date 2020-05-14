75a0f952b4d409c23a921b807ab658d1babe5f2e
Kewbish <kewbish@gmail.com>
2020-05-13 19:36:28 -0700

    Add basic pointer functionality

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+document.allPointers = []
+
 class RevShare extends HTMLElement{
 
     connectedCallback(){
+        this.style.margin = "0";
+        this.style.padding = "0";
         console.log('New Revshare element created.');
+        var parsedPointers = this.getAttribute('pointers');
+        parsedPointers = JSON.parse(parsedPointers);
+        document.allPointers.push(parsedPointers);
     }
-
 }
 
 window.customElements.define('rev-share', RevShare);
