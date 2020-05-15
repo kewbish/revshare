e69e2e800962c2d7c68aea94ebe009a5e877d702
Kewbish <kewbish@gmail.com>
2020-05-14 19:34:22 -0700

    Move to getting all of tagname

--- index.js
+++ index.js
@@ -6,31 +6,36 @@ class RevShare extends HTMLElement {
         this.style.margin = "0";
         this.style.padding = "0";
         console.log('New Revshare element created.');
-        var parsedPointers = JSON.parse(this.getAttribute('pointers'));
-        var revId = this.getAttribute('rev-id');
-        document.allPointers[revId] = parsedPointers;
     }
     attributeChangedCallback() {
-        var parsedPointers = JSON.parse(this.getAttribute('pointers'));
-        var revId = this.getAttribute('rev-id');
-        document.allPointers[revId] = parsedPointers;
+        this.checkParsedPointers();
         genMeta();
     }
     static get observedAttributes() {
         return ['pointers'];
+    }
+    checkParsedPointers() {
+        var parsedPointers = JSON.parse(this.getAttribute('pointers'));
+        if (Object.values(parsedPointers).reduce((a, b) => a + b) != 100) {
+            console.warn('RevShareProportionWarning: revshare proportions do not sum to 100. Check that these proportions are correct');
+        }
     }
 }
 
 window.customElements.define('rev-share', RevShare);
 
 function pickPointer() {
-    var p = Math.floor(Math.random() * document.allPointers.length);
-    const sum = document.allPointers[p] && Object.values(document.allPointers[p]).reduce((sum, weight) => sum + weight, 0);
+    var revEls = document.getElementsByTagName('rev-share');
+    var p = Math.floor(Math.random() * revEls.length);
+    const sum = revEls[p] && Object.values(revEls[p]).reduce((sum, weight) => sum + weight, 0);
     let choice = Math.random() * sum;
-    for (const pointer in document.allPointers[p]) {
-        const weight = document.allPointers[p][pointer];
-        if ((choice -= weight) <= 0) {
-            return pointer;
+    if (typeof revEls[p] !== "undefined") {
+        var choices = JSON.parse(revEls[p].getAttribute('pointers'));
+        for (const pointer in choices) {
+            const weight = choices[pointer];
+            if ((choice -= weight) <= 0) {
+                return pointer;
+            }
         }
     }
 }
